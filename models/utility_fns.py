08f31fa2c9abe1f72bbe8986edc29a31e6c2efcb
Eric Hall <ewhall18@gmail.com>
2021-12-05 00:15:32 -0500

    no really, this is the penultimate push

--- models/utility_fns.py
+++ models/utility_fns.py
@@ -8,7 +8,7 @@ from sklearn.utils.fixes import threadpool_info
 from sys import stdout
 
 
-def form_last_n_games(df: pd.DataFrame, n: int, cols_to_grab=['Class'], lookup_cols=['Team Code']):
+def form_last_n_games(df: pd.DataFrame, n: int, cols_to_grab=['Class'], lookup_cols=['Team Code'], filter_col='Date'):
     """
     Requires that the input df is sorted such that old elements are at the top,
     e.g. `df = df.sort_values(by='Date', ascending=True)`
@@ -20,6 +20,7 @@ def form_last_n_games(df: pd.DataFrame, n: int, cols_to_grab=['Class'], lookup_c
 
     result = df.copy()
 
+    cols_to_grab += [filter_col]
     new_columns = [lookup + '_' + ''.join(['p']*(prev+1)) + '_' +
                    col for col in cols_to_grab for prev in range(n) for lookup in lookup_cols]
     new_df = pd.DataFrame(columns=new_columns)
@@ -30,7 +31,7 @@ def form_last_n_games(df: pd.DataFrame, n: int, cols_to_grab=['Class'], lookup_c
         to_drop = False
         append_row = pd.Series(index=new_columns, name=index)
         for lookup in lookup_cols:
-            lkup_key = row[lookup]
+            lkup_key = lookup + row[lookup]
             last_n = lookup_dict.get(lkup_key, deque(maxlen=n))
 
             if len(last_n) < n:
@@ -44,6 +45,8 @@ def form_last_n_games(df: pd.DataFrame, n: int, cols_to_grab=['Class'], lookup_c
                     #     lookup + '_' + ''.join(['p']*(prev+1)) + '_' + col for col in cols_to_grab for prev in range(n)]
                     # append_row[append_cols] = [elem for r in last_n for elem in r[cols_to_grab]]
 
+            if len(last_n) != 0 and row[filter_col] == last_n[0][filter_col]:
+                continue
             last_n.appendleft(row[cols_to_grab])
             lookup_dict[lkup_key] = last_n
 
